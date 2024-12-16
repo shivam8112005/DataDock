5f4ecd4dd788515b0b6c3d007c1c520065297c4c
shivam shukla <shivamshukla8112005@gmail.com>
2024-12-16 22:35:25 +0530

    added mongoose modes and routes for notes and auth

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,10 @@ connectToMong();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
