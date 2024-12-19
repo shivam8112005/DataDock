1583ab5cf34d5bebd1383b6a9f70b172816a569f
shivam shukla <shivamshukla8112005@gmail.com>
2024-12-19 18:56:42 +0530

    added collections in thunderclient and altered authentications endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -32,9 +32,9 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-// Create a user using POST "/api/auth". Doesn't require authentication.
+// Create a user using POST "/api/auth/createuser". Doesn't require authentication.
 router.post(
-  '/',
+  '/createuser',
   [
     // Validation for request body
     body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
@@ -43,6 +43,7 @@ router.post(
   ],
   async (req, res) => {
     // Validate the request
+    //if there are errors return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // If validation fails, return a 400 response with errors
@@ -51,7 +52,12 @@ router.post(
 
     try {
       // Create a new user in the database
-      const user = await User.create({
+      //check whether the email exists already
+      let user=await User.findOne({email:req.body.email});
+      if(user){
+        return res.status(400).json({error: "user with this email already exists"})
+      }
+       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
